64ab65c7e46aa1b505d2bdc94c0030b05f635e93
Ayushman Singh Sisodiya <ayushmanss@juniper.net>
2018-01-08 00:24:05 +0530

    testing

--- test.py
+++ test.py
@@ -7,6 +7,7 @@ test_folder = 'rules'
 
 rules = os.listdir(test_folder)
 
+open(outfile, 'w').close()
 with open(outfile, 'a') as f:
     for rule in rules:
         testfile_path = os.path.join(test_folder, rule)
