b373aea0c8c532ef127f1b4449719ad0be78e79a
Ayushman Singh Sisodiya <ayushmanss@juniper.net>
2018-01-07 14:40:43 +0530

    added support for status back to github, needs description refinement

---
+++ post_status.py
@@ -0,0 +1,22 @@
+import os
+import github
+import subprocess
+
+sha = os.popen('git rev-parse HEAD').read().strip()
+g = github.Github("ayushgithub", "Ayushman1")
+
+status_file = 'status.out'
+with open(status_file, 'r') as f:
+    description = f.read()
+
+if len(description) != 0:
+    state = 'failure'
+    description = 'test failed'
+else:
+    state = 'success'
+    description = 'test passed'
+
+repo = g.get_repo('ayushgithub/jfit-rules')
+commit = repo.get_commit(sha)
+
+ret = commit.create_status(state=state, target_url="https://www.google.com", description=description)
