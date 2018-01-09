360f2aeb608b3c1911064c7798c7540056d1d1ea
Ayushman Singh Sisodiya <ayushmanss@juniper.net>
2018-01-09 12:34:32 +0530

    removed target url, need to find other way

--- post_status.py
+++ post_status.py
@@ -5,14 +5,10 @@ import ftplib
 
 sha = os.popen('git rev-parse HEAD').read().strip()
 g = github.Github("ayushgithub", "Ayushman1")
-session = ftplib.FTP('files.000webhost.com','ayushmansisodiya','Ayushman1')
-session.cwd('public_html')
 
 status_file = 'status.out'
 with open(status_file, 'r') as f:
     description = f.read()
-with open(status_file, 'r') as f:
-    session.storbinary('STOR ' + sha + '.txt', f)
 
 print description, len(description)
 if len(description) != 0:
@@ -25,5 +21,4 @@ else:
 repo = g.get_repo('ayushgithub/jfit-rules')
 commit = repo.get_commit(sha)
 
-ret = commit.create_status(state=state, target_url='https://ayushmansisodiya.000webhostapp.com/'+sha+'.txt', description=description)
-session.quit()
+ret = commit.create_status(state=state, target_url='', description=description)
